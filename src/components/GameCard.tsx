41f1ad86726d9b1893728f4d087b61ab16ad0c9d
Nicolas Cnudde <nicolas.cnudde@icloud.com>
2025-04-29 11:08:13 +0200

    Slightly adjust the game card styling

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -8,9 +8,9 @@ interface Props {
 const GameCard = ({ game }: Props) => {
 	return (
 		<Card.Root borderRadius={10} overflow="hidden">
-			<Image src={game.background_image} />
+			<Image width="100%" height="200px" objectFit="cover" src={game.background_image} />
 
-			<Card.Body bg="darkslategray">
+			<Card.Body>
 				<Heading fontSize="2xl">{game.name}</Heading>
 			</Card.Body>
 		</Card.Root>
